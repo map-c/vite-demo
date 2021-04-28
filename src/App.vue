ecee078a550939fcc975aed35b30a7c8b63f034c
cola <colablog@163.com>
2021-04-28 09:22:54 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="text-sm">hellow vue</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
